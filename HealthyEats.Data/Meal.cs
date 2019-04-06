b565d580ffbd97759243fda32465f48ad767d88b
Spriggs01 <sarah.spriggs@gamil.com>
2019-04-06 19:36:48 -0400

    set up connection string

---
+++ HealthyEats.Data/Meal.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthyEats.Data
+{
+    public enum MealType
+    {
+        mealType, MealDescription
+
+    }
+
+    public class Meal
+    {
+        [Required]
+        public Guid UserID { get; set; }
+
+        [Required]
+        public int RecipeID { get; set; }
+
+        [Key]
+        public int MealID { get; set; }
+
+        [Required]
+        public MealType TypeOfMeal { get; set; }
+    }
+}
