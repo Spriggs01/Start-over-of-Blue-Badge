b1de2da4039ac4e56c6cc3a4a57f06272fb64f78
Spriggs01 <sarah.spriggs@gamil.com>
2019-04-07 17:32:52 -0400

    set up Services

--- HealthyEats.Services/FavoriteRecipeService.cs
+++ HealthyEats.Services/FavoriteRecipeService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using HealthyEats.WebMVC.Data;
+using HealtyEats.Models;
+using HealtyEats.WebMVC.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,53 @@ using System.Threading.Tasks;
 
 namespace HealthyEats.Services
 {
-    class FavoriteRecipeService
+    public class FavoriteRecipeService
     {
+
+        private readonly int _favoriteRecipeId; 
+
+        public FavoriteRecipeService(int favoriteRecipeId)
+        {
+            _favoriteRecipeId = favoriteRecipeId;
+        }
+
+        public bool CreateFavoriteRecipe(FavoriteRecipeCreate model)
+        {
+            var entity =
+                new FavoriteRecipe()
+                {
+                    FavoriteRecipeID = _favoriteRecipeId,
+                    FavoriteList = model.FavoriteList,
+                    Recipes = model.Recipes
+                };
+
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.FavoriteRecipes.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public IEnumerable<FavoriteRecipeListItem> GetFavoriteRecipe()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                    .FavoriteRecipes
+                    .Where(e => e.FavoriteRecipeID == _favoriteRecipeId)
+                    .Select(
+                        e =>
+                        new FavoriteRecipeListItem
+                        {
+                            FavoriteRecipeID = e.FavoriteRecipeID,
+                            FavoriteList = e.FavoriteList,
+                            Recipes = e.Recipes
+                        }
+                        );
+                return query.ToArray();
+            }
+        }
+
     }
 }
--- HealthyEats.Services/RecipeService.cs
+++ HealthyEats.Services/RecipeService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using HealthyEats.Data;
+using HealthyEats.WebMVC.Data;
+using HealtyEats.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,62 @@ using System.Threading.Tasks;
 
 namespace HealthyEats.Services
 {
-    class Recipe
+    public class RecipeService
     {
+        public readonly int _recipeID;
+
+        public RecipeService(int recipeId)
+        {
+            _recipeID = recipeId;
+        }
+        public bool CreateRecipe(RecipeCreate model)
+        {
+            var entity =
+                new Recipe()
+                {
+                    RecipeID = _recipeID,
+                    RecipeTitle = model.RecipeTitle,
+                    Link = model.Link,
+                    Calories = model.Calories,
+                    TypeName = model.TypeName,
+                    Dietary = model.Dietary,
+                    Meals = model.Meals,
+                    FavoriteRecipes = model.FavoriteRecipes
+
+                };
+
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Recipes.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public IEnumerable<RecipeListItem> GetRecipe()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                    .Recipes
+                    .Where(e => e.RecipeID == _recipeID)
+                    .Select(
+                        e =>
+                        new RecipeListItem
+                        {
+                            RecipeTitle = e.RecipeTitle,
+                            Link = e.Link,
+                            Calories = e.Calories,
+                            TypeName = e.TypeName,
+                            Dietary = e.Dietary,
+                            Meals = e.Meals,
+                            FavoriteRecipes = e.FavoriteRecipes
+                        }
+                        );
+
+                return query.ToArray();
+            }
+        }
+
     }
 }
