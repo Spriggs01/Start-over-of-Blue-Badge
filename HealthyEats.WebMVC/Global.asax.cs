980dcb295a3e9dd0349730bb0b689255ba526790
Spriggs01 <sarah.spriggs@gamil.com>
2019-04-22 11:06:45 -0400

    more styling

--- HealthyEats.WebMVC/Global.asax.cs
+++ HealthyEats.WebMVC/Global.asax.cs
@@ -16,6 +16,7 @@ namespace HealthyEats.WebMVC
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            BundleTable.EnableOptimizations = false;
         }
     }
 }
