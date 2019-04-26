78d6e6228efa3c8190f21bd054e100c1359d564e
Spriggs01 <sarah.spriggs@gamil.com>
2019-04-26 11:03:10 -0400

    added a controller

---
+++ HealthyEats.WebAPI/Controllers/HealthyEatsController.cs
@@ -0,0 +1,28 @@
+﻿using HealthyEats.Services;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace HealthyEats.WebAPI.Controllers
+{
+    [Authorize]
+    public class HealthyEatsController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            MealService mealService = CreateMealService();
+            var meals = mealService.GetMeal();
+            return Ok(meals);
+        }
+        private MealService CreateMealService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var mealService = new MealService(userId);
+            return mealService;
+        }
+    }
+}
