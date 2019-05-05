390aa6deb9f8ba17f8b781202145e39b67400164
Spriggs01 <sarah.spriggs@gamil.com>
2019-05-04 20:15:29 -0400

    added Angular

---
+++ HealthyEatsAngular/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatToolbarModule, MatButtonModule, MatFormFieldModule, MatInputModule} from '@angular/material';
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { RegistrationComponent } from './components/registration/registration.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    RegistrationComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+   FormsModule,
+    ReactiveFormsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
